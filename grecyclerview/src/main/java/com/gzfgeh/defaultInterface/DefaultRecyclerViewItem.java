f9e159e5068c14ef6349742500f98a34fbc09884
guzhenfu <guzhenfu@shcem.com>
2016-11-28 20:29:00 +0800

    first commit

---
+++ grecyclerview/src/main/java/com/gzfgeh/defaultInterface/DefaultRecyclerViewItem.java
@@ -0,0 +1,17 @@
+package com.gzfgeh.defaultInterface;
+
+import android.view.View;
+
+import com.gzfgeh.adapter.RecyclerArrayAdapter;
+
+/**
+ * Description:
+ * Created by guzhenfu on 2016/10/27 14:01.
+ */
+
+public abstract class DefaultRecyclerViewItem implements RecyclerArrayAdapter.ItemView {
+    @Override
+    public void onBindView(View headerView) {
+
+    }
+}
